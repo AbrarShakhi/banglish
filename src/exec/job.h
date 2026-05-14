c0863eac1cf9557cd81c53e234af2b5aeebcc508
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-05-14 16:08:17 +0600

    add job struct

--- src/exec/job.h
+++ src/exec/job.h
@@ -6,3 +6,17 @@ typedef enum {
 	JOB_STOPPED,
 	JOB_DONE,
 } JobStatus;
+
+typedef struct Job {
+	int jid;    /* 1-based job ID              */
+	pid_t pgid; /* process group ID            */
+	JobStatus status;
+	int notified;  /* 1 if user was told it ended */
+	char *cmdline; /* display string; heap-owned  */
+	struct Job *next;
+} Job;
+
+typedef struct JobTable {
+	Job *head;
+	int next_jid;
+} JobTable;
