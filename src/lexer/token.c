b377346a13accf935ec844de374b07f67558f32e
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-05-15 02:02:17 +0600

    fix: bug on tok->value = value . type missmatch

--- src/lexer/token.c
+++ src/lexer/token.c
@@ -1,10 +1,11 @@
 #include "token.h"
 #include "utils/memory.h"
+#include <stdlib.h>
 
 Token *token_new(TokenType type, const char *value, int line, int col) {
   Token *tok = mem_alloc(sizeof(Token));
   tok->type = type;
-  tok->value = value;
+  tok->value = value ? mem_strdup(value) : NULL;
   tok->line = line;
   tok->col = col;
   return tok;
